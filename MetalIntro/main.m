446b0d290637e545fa18ffc163aec05c6550d8de
Josh <36666883+Hibyehello@users.noreply.github.com>
2023-05-10 20:47:10 -0500

    Initial Commit

---
+++ MetalIntro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MetalIntro
+//
+//  Created by Hibyehello on 5/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
